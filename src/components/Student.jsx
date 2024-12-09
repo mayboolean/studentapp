e40490eeaefdc1488c17ff552d5c0763002d66a1
mayboolean <>
2024-12-08 20:07:22 -0500

    added toggle function

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,11 +1,34 @@
-/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Student.css';
+
 const Student = props => {
+  // set up the state variable and function
+  const [isPresent, setIsPresent] = useState(false);
+  
+  // event handler function
+  const togglePresence = () => {
+    setIsPresent(!isPresent);
+  };
+
+  // assign class name depending on isPresent state
+  const nameColor = isPresent ? 'isPresentTrue' : 'isPresentFalse'
+
   return (
-    <ul>
-      <li>Name:{props.name}</li>
-      <li>email:{props.email}</li>
-    </ul>
+    <div>
+      <ul className={nameColor}>
+        <li>Name:{props.name}</li>
+        <li>email:{props.email}</li>
+      </ul>
+      <button onClick={togglePresence}>Toggle for {props.name}&apos;s Presence status</button>
+    </div> 
+    
   )
+};
+
+Student.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired
 };
 
 export default Student;
